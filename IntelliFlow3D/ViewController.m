b8aa79bdaf5f8d6226a640e9f2c92651e6d3e5ac
todisht <toddtodish@TTs-MacBook-Pro.local>
2014-03-21 16:42:51 -0400

    added image array animation working - turned off for now.

--- IntelliFlow3D/ViewController.m
+++ IntelliFlow3D/ViewController.m
@@ -17,7 +17,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    // Load images
+    NSArray *imageNames = @[@"png/App_Openup_object_1_00000.png",@"png/App_Openup_object_1_00001.png",@"png/App_Openup_object_1_00002.png",@"png/App_Openup_object_1_00003.png",@"png/App_Openup_object_1_00004.png",@"png/App_Openup_object_1_00005.png",@"png/App_Openup_object_1_00006.png",@"png/App_Openup_object_1_00007.png",@"png/App_Openup_object_1_00008.png",@"png/App_Openup_object_1_00009.png",@"png/App_Openup_object_1_00010.png",@"png/App_Openup_object_1_00011.png",@"png/App_Openup_object_1_00012.png",@"png/App_Openup_object_1_00013.png",@"png/App_Openup_object_1_00014.png",@"png/App_Openup_object_1_00015.png",@"png/App_Openup_object_1_00016.png",@"png/App_Openup_object_1_00017.png",@"png/App_Openup_object_1_00018.png",@"png/App_Openup_object_1_00019.png",@"png/App_Openup_object_1_00020.png",@"png/App_Openup_object_1_00021.png",@"png/App_Openup_object_1_00022.png",@"png/App_Openup_object_1_00023.png",@"png/App_Openup_object_1_00024.png",@"png/App_Openup_object_1_00025.png",@"png/App_Openup_object_1_00026.png",@"png/App_Openup_object_1_00027.png",@"png/App_Openup_object_1_00028.png",@"png/App_Openup_object_1_00029.png",@"png/App_Openup_object_1_00030.png",@"png/App_Openup_object_1_00031.png"];
+    
+    NSMutableArray *images = [[NSMutableArray alloc] init];
+    for (int i = 0; i < imageNames.count; i++) {
+        [images addObject:[UIImage imageNamed:[imageNames objectAtIndex:i]]];
+    }
+    
+    // Normal Animation
+    UIImageView *animationImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 1024, 768)];
+    animationImageView.animationImages = images;
+    animationImageView.animationDuration = 0.5;
+    
+    [self.view addSubview:animationImageView];
+    //[animationImageView startAnimating];
 }
 
 - (void)didReceiveMemoryWarning
